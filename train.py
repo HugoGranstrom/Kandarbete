7fa9679aeedcde9470a82da3350ebf0533eac19c
HugoGranstrom <5092565+HugoGranstrom@users.noreply.github.com>
2021-04-16 19:40:09 +0200

    GANs!!!!!!

--- train.py
+++ train.py
@@ -38,30 +38,32 @@ import torch
 from torchvision import models
 
 class AdverserialModel(nn.Module):
-  def __init__(self, high_res):
+  def __init__(this, high_res):
     super().__init__()
     this.model = nn.Sequential(
-    nn.Conv2d(3, 16, 3,padding=1), # 3*3*3*16 = 432
-    nn.ReLU(),
-    nn.Conv2d(16, 32, 3,padding=1, stride=2), # 4608
-    nn.ReLU(),
-    nn.Conv2d(32, 64, 3,padding=1, stride=2), # 18 432
-    nn.ReLU(),
-    nn.Conv2d(64, 128, 3,padding=1, stride=2), # 73 728
-    nn.ReLU(),
-    nn.Conv2d(128, 128, 3,padding=1, stride=2), # 147 456
-    nn.ReLU(),
-    nn.Conv2d(128, 128, 3,padding=1, stride=2), # 147 456
-    nn.ReLU(),
-    nn.Linear(128*high_res*high_res/1024, 1024), # 8 388 608
-    nn.ReLU(),
-    nn.Linear(1024, 128),
-    nn.ReLU(),
-    nn.Linear(128, 1))
-    nn.Sigmoid(),
+      nn.Conv2d(3, 16, 3,padding=1), # 3*3*3*16 = 432
+      nn.LeakyReLU(0.2, inplace=True),
+      nn.Conv2d(16, 32, 3,padding=1, stride=2), # 4608
+      nn.LeakyReLU(0.2, inplace=True),
+      nn.Conv2d(32, 64, 3,padding=1, stride=2), # 18 432
+      nn.LeakyReLU(0.2, inplace=True),
+      nn.Conv2d(64, 128, 3,padding=1, stride=2), # 73 728
+      nn.LeakyReLU(0.2, inplace=True),
+      nn.Conv2d(128, 128, 3,padding=1, stride=2), # 147 456
+      nn.LeakyReLU(0.2, inplace=True),
+      nn.Conv2d(128, 128, 3,padding=1, stride=2), # 147 456
+      nn.LeakyReLU(0.2, inplace=True),
+      nn.Flatten(),
+      nn.Linear(int(128*high_res*high_res/1024), 1024), # 8 388 608
+      nn.LeakyReLU(0.2, inplace=True),
+      nn.Linear(1024, 128),
+      nn.LeakyReLU(0.2, inplace=True),
+      nn.Linear(128, 1),
+      nn.Sigmoid()
+    )
 
-  def forward(self, x):
-    return model(x)
+  def forward(this, x):
+    return this.model(x)
 
 if __name__ == '__main__':
   torch.multiprocessing.freeze_support()
@@ -81,10 +83,15 @@ if __name__ == '__main__':
   lr_max = 0.0005
 
   net = UNet(depth=5).to(device)
-  optimizer = torch.optim.Adam(net.parameters(), lr=lr_min)
+  optimizer = torch.optim.Adam(net.parameters(), lr=0.0002)
   
-  disc = AdverserialModel().to(device)
-  optimizer_disc = torch.optim.Adam(net.parameters(), lr=lr_min)
+  disc = AdverserialModel(256).to(device)
+  optimizer_disc = torch.optim.Adam(disc.parameters(), lr=0.0002)
+
+  criterion = nn.BCELoss()
+
+  real_label = 1.
+  fake_label = 0.
 
   filename = "net_UNet.pt"
 
@@ -92,14 +99,14 @@ if __name__ == '__main__':
   best_loss = min(val_losses) if len(val_losses) > 0 else 1e6
   print("Best validation loss:", best_loss)
   iteration = iterations[-1] if len(iterations) > 0 else -1
-  scheduler = torch.optim.lr_scheduler.CyclicLR(optimizer, base_lr=lr_min, max_lr=lr_max, step_size_up=2000, last_epoch=iteration, mode="triangular", cycle_momentum=False)
+  #scheduler = torch.optim.lr_scheduler.CyclicLR(optimizer, base_lr=lr_min, max_lr=lr_max, step_size_up=2000, last_epoch=iteration, mode="triangular", cycle_momentum=False)
 
   net.train()
   net.to(device)
   validation_size = 100
-
-  dataset = OpenDataset(ids[:-validation_size], batch_size=15, SUPER_BATCHING=40, high_res_size=(256, 256), low_res_size=(128, 128))
-  validation_dataset = OpenDataset(ids[-validation_size:], batch_size=15, SUPER_BATCHING=1, high_res_size=(256, 256), low_res_size=(128, 128))
+  batch_size = 10
+  dataset = OpenDataset(ids[:-validation_size], batch_size=batch_size, SUPER_BATCHING=40, high_res_size=(256, 256), low_res_size=(128, 128))
+  validation_dataset = OpenDataset(ids[-validation_size:], batch_size=batch_size, SUPER_BATCHING=1, high_res_size=(256, 256), low_res_size=(128, 128))
   validation_data = [i for i in validation_dataset]
   validation_size = len(validation_data)
   """traindata = FolderSet("train")
@@ -114,32 +121,55 @@ if __name__ == '__main__':
   save_every = 1
   for epoch in range(1000):  # loop over the dataset multiple times
 
-      running_loss = 0.0
+      running_lossD = 0.0
+      running_lossG = 0.0
       train_loss = 0.0
       for i, data in enumerate(dataset, iteration+1):
           # get the inputs; data is a list of [inputs, labels]
-          inputs, labels = data
+          inputs, real = data
           inputs = inputs.to(device)
-          labels = labels.to(device)
+          real = real.to(device)
           # zero the parameter gradients
-          optimizer.zero_grad()
+          optimizer_disc.zero_grad()
+
+          real_labels = torch.full((batch_size,), real_label, dtype=torch.float, device=device)
+          output = disc(real)
+          errD_real = criterion(output.squeeze(), real_labels.squeeze())
+          errD_real.backward()
 
           # forward + backward + optimize
-          outputs = net(inputs)
+          fakes = net(inputs)
+          fake_labels = torch.full((batch_size,), fake_label, dtype=torch.float, device=device)
+          output = disc(fakes.detach()).view(-1)
+          errD_fake = criterion(output.squeeze(), fake_labels.squeeze())
+          errD_fake.backward()
+          errD = errD_real + errD_fake
+          optimizer_disc.step()
 
-          loss = perceptual_loss(outputs, labels, vgg)
-          loss += F.l1_loss(outputs, labels)
-          loss.backward()
+          optimizer.zero_grad()
+          output = disc(fakes).view(-1)
+          errG = criterion(output, real_labels)
+          errG.backward()
           optimizer.step()
-          scheduler.step()
-          running_loss += loss.item()
-          train_loss += loss.item()
+
+          errorD = errD.mean().item()
+          errorG = errG.mean().item()
+          #loss = perceptual_loss(outputs, real, vgg)
+          #loss += F.l1_loss(outputs, real)
+          #loss.backward()
+          #optimizer.step()
+          #scheduler.step()
+          running_lossG += errorG
+          running_lossD += errorD
+          train_loss += errorG + errorD
           # print statistics
           if i % print_every == print_every-1:
-              print('[%d, %5d] loss: %.4f' %
-                    (epoch, i, running_loss / print_every))
-              running_loss = 0.0
-          if i % save_every == save_every-1:
+              print('[%d, %5d] lossG: %.4f' %
+                    (epoch, i, running_lossG / print_every))
+              print('[%d, %5d] lossD: %.4f' %
+                    (epoch, i, running_lossD / print_every))
+              running_lossD, running_lossG = 0.0, 0.0
+          """if i % save_every == save_every-1:
             train_losses.append(train_loss / save_every)
             print("Training loss:", train_loss / save_every)
             train_loss = 0.0
@@ -169,6 +199,6 @@ if __name__ == '__main__':
                 print(f"New best loss: {best_loss} -> {validation_loss}")
                 best_loss = validation_loss
               saveNet(filename, net, optimizer, iterations, train_losses, val_losses)
-              print("Saved model!")
+              print("Saved model!")"""
               
                 
