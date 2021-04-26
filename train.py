525b60981007b714898c624cb655bbef2f4bd273
Caspar <cachpachios@gmail.com>
2021-04-26 14:24:29 +0200

    VGG Transfered

--- train.py
+++ train.py
@@ -38,104 +38,6 @@ from collections import namedtuple
 import torch
 from torchvision import models
 
-class VGG(nn.Module):
-    """VGG/Perceptual Loss
-    
-    Parameters
-    ----------
-    conv_index : str
-        Convolutional layer in VGG model to use as perceptual output
-
-    """
-    def __init__(self, conv_index: str = '22'):
-
-        super(VGG, self).__init__()
-        vgg_features = models.vgg19(pretrained=True).features
-        modules = [m for m in vgg_features]
-        
-        if conv_index == '22':
-            self.vgg = nn.Sequential(*modules[:9])
-        elif conv_index == '54':
-            self.vgg = nn.Sequential(*modules[:36])
-
-        vgg_mean = (0.485, 0.456, 0.406)
-        vgg_std = (0.229, 0.224, 0.225)
-        #self.sub_mean = common.MeanShift(rgb_range, vgg_mean, vgg_std)
-        self.vgg.requires_grad = False
-        for param in self.parameters():
-          param.requires_grad = False
-
-
-    def calcLoss(self, sr: torch.Tensor, hr: torch.Tensor) -> torch.Tensor:
-        """Compute VGG/Perceptual loss between Super-Resolved and High-Resolution
-
-        Parameters
-        ----------
-        sr : torch.Tensor
-            Super-Resolved model output tensor
-        hr : torch.Tensor
-            High-Resolution image tensor
-
-        Returns
-        -------
-        loss : torch.Tensor
-            Perceptual VGG loss between sr and hr
-
-        """
-        def _forward(x):
-            #x = self.sub_mean(x)
-            x = self.vgg(x)
-            return x
-            
-        vgg_sr = _forward(sr)
-
-        with torch.no_grad():
-            vgg_hr = _forward(hr.detach())
-
-        loss = F.mse_loss(vgg_sr, vgg_hr)
-
-        return loss
-
-def perceptual_loss(real, fake, vgg):
-  """Normalizes y and y_hat, runs them through vgg and compares intermediate layers and returns the perceptual loss"""
-  mean = torch.tensor([0.485, 0.456, 0.406])
-  std = torch.tensor([0.229, 0.224, 0.225]) # the biggest value that can be normalized to is 2.64
-  normalize = transforms.Normalize(mean.tolist(), std.tolist())
-  unnormalize = transforms.Normalize((-mean / std).tolist(), (1.0 / std).tolist())
-
-  #features_y = vgg(normalize(y))
-  #features_y_hat = vgg(normalize(y_hat))
-  #loss = 0.5 * F.mse_loss(features_y_hat.relu2_2, features_y.relu2_2)
-  loss = 0.5 * vgg.calcLoss(normalize(fake), normalize(real))
-  return loss
-
-class AdverserialModel(nn.Module):
-  def __init__(this, high_res):
-    super().__init__()
-    this.model = nn.Sequential(
-      nn.Conv2d(3, 16, 3,padding=1), # 3*3*3*16 = 432
-      nn.LeakyReLU(0.2, inplace=True),
-      nn.Conv2d(16, 32, 3,padding=1, stride=2), # 4608
-      nn.LeakyReLU(0.2, inplace=True),
-      nn.Conv2d(32, 64, 3,padding=1, stride=2), # 18 432
-      nn.LeakyReLU(0.2, inplace=True),
-      nn.Conv2d(64, 128, 3,padding=1, stride=2), # 73 728
-      nn.LeakyReLU(0.2, inplace=True),
-      nn.Conv2d(128, 128, 3,padding=1, stride=2), # 147 456
-      nn.LeakyReLU(0.2, inplace=True),
-      nn.Conv2d(128, 128, 3,padding=1, stride=2), # 147 456
-      nn.LeakyReLU(0.2, inplace=True),
-      nn.Flatten(),
-      nn.Linear(int(128*high_res*high_res/1024), 1024), # 8 388 608
-      nn.LeakyReLU(0.2, inplace=True),
-      nn.Linear(1024, 128),
-      nn.LeakyReLU(0.2, inplace=True),
-      nn.Linear(128, 1),
-      nn.Sigmoid()
-    )
-
-  def forward(this, x):
-    return this.model(x)
 
 if __name__ == '__main__':
   torch.multiprocessing.freeze_support()
@@ -149,7 +51,7 @@ if __name__ == '__main__':
   print('cuda' if torch.cuda.is_available() else 'cpu')
 
   device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
+  #device = torch.device('cpu')
 
   lr_min = 0.0001
   lr_max = 0.0005
@@ -157,11 +59,18 @@ if __name__ == '__main__':
   net = UNet(depth=5).to(device)
   optimizer = torch.optim.Adam(net.parameters(), lr=0.0002)
   
-  disc = AdverserialModel(256).to(device)
+  disc = models.vgg16(pretrained=True)
+    
+  for param in disc.features.parameters():
+    param.require_grad = False
+      
+  num_features = disc.classifier[6].in_features
+  disc.classifier[-1] =  nn.Linear(num_features, 1)
+  disc.to(device)
+  
   optimizer_disc = torch.optim.Adam(disc.parameters(), lr=0.0002)
 
   criterion = nn.BCELoss()
-  vgg = VGG(conv_index="54").to(device).eval()
 
   real_label = 1.
   fake_label = 0.
@@ -177,7 +86,7 @@ if __name__ == '__main__':
   net.train()
   net.to(device)
   validation_size = 100
-  batch_size = 15
+  batch_size = 8
 
   dataset = OpenDataset(ids[:-validation_size], batch_size=batch_size, SUPER_BATCHING=40, high_res_size=(256, 256), low_res_size=(128, 128))
   validation_dataset = OpenDataset(ids[-validation_size:], batch_size=batch_size, SUPER_BATCHING=1, high_res_size=(256, 256), low_res_size=(128, 128))
@@ -191,6 +100,10 @@ if __name__ == '__main__':
   validation_data = DataLoader(validdata, batch_size=15)
   validation_size = len(validation_data)
   """
+  mean = torch.tensor([0.485, 0.456, 0.406])
+  std = torch.tensor([0.229, 0.224, 0.225]) # the biggest value that can be normalized to is 2.64
+  normalize = transforms.Normalize(mean.tolist(), std.tolist())
+  
   
   print_every = 100
   save_every = 500
@@ -212,25 +125,24 @@ if __name__ == '__main__':
           batch_size = len(inputs)
           
           real_labels = torch.full((batch_size,), real_label, dtype=torch.float, device=device)
-          output = disc(real)
+          output = torch.sigmoid(disc(normalize(real)))
           errD_real = criterion(output.squeeze(), real_labels.squeeze())
           errD_real.backward()
 
           # forward + backward + optimize
           fakes = net(inputs)
           fake_labels = torch.full((batch_size,), fake_label, dtype=torch.float, device=device)
-          output = disc(fakes.detach()).view(-1)
+          output = torch.sigmoid(disc(normalize(fakes.detach())).view(-1))
           errD_fake = criterion(output.squeeze(), fake_labels.squeeze())
           errD_fake.backward()
           errD = errD_real + errD_fake
           optimizer_disc.step()
 
           optimizer.zero_grad()
-          output = disc(fakes).view(-1)
+          output = torch.sigmoid(disc(normalize(fakes)).view(-1))
           errG = criterion(output, real_labels)
           loss = 1e-3 * errG
-          loss += perceptual_loss(real, fakes, vgg)
-          loss += F.mse_loss(real, fakes)
+          loss += F.l1_loss(real, fakes)
           loss.backward()
           optimizer.step()
 
@@ -267,8 +179,8 @@ if __name__ == '__main__':
                 inputs = inputs.to(device)
                 labels = labels.to(device)
                 outputs_val = net(inputs)
-                per_loss = perceptual_loss(labels, outputs_val, vgg)
                 pix_loss = F.l1_loss(outputs_val, labels)
+                per_loss = pix_loss #Temp
                 percep_loss += per_loss.item()
                 pixel_loss += pix_loss.item()
 
