cf6e7c06d9d70c607f6f7b33cdef590bf976c0d1
HugoGranstrom <5092565+HugoGranstrom@users.noreply.github.com>
2021-05-07 14:35:53 +0200

    vgg19 before activations

--- losses.py
+++ losses.py
@@ -56,9 +56,9 @@ class VGG(nn.Module):
         modules = [m for m in vgg_features]
         
         if conv_index == '22':
-            self.vgg = nn.Sequential(*modules[:9])
+            self.vgg = nn.Sequential(*modules[:8])
         elif conv_index == '54':
-            self.vgg = nn.Sequential(*modules[:36])
+            self.vgg = nn.Sequential(*modules[:35])
 
         vgg_mean = (0.485, 0.456, 0.406)
         vgg_std = (0.229, 0.224, 0.225)
